516ae92802e0d6ef8b7d487808ad37109404c3c9
Atish03 <atishpersonal003@gmail.com>
2025-02-09 00:16:49 +0530

    Added index to published_at to increase query retrieval speed

--- init_scripts/init.sql
+++ init_scripts/init.sql
@@ -1,8 +1,10 @@
 -- Create the table if it doesn't exist
 CREATE TABLE IF NOT EXISTS videos (
-    video_id TEXT PRIMARY KEY,
+    video_id TEXT UNIQUE,
     title TEXT NOT NULL,
     description TEXT,
     published_at TIMESTAMP WITH TIME ZONE NOT NULL,
     thumbnail_url TEXT
 );
+
+CREATE INDEX IF NOT EXISTS idx_published_at ON videos (published_at DESC);
